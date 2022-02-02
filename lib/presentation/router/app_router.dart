d1e5adacf90271a4e2b7103fd425b6f2276136ce
Amgad22318 <64033577+Amgad22318@users.noreply.github.com>
2022-02-02 18:37:34 +0200

    amgad last code 2.05

--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -2,21 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:productive_families/constants/end_points.dart';
 import 'package:productive_families/presentation/screens/about_product/about_product.dart';
 import 'package:productive_families/presentation/screens/about_us/about_us.dart';
+import 'package:productive_families/presentation/screens/filter_screens/filtering_screen.dart';
 import 'package:productive_families/presentation/screens/login/login_screen.dart';
 import 'package:productive_families/presentation/screens/meal/meals_screen.dart';
 import 'package:productive_families/presentation/screens/orders/orders_screen.dart';
 import 'package:productive_families/presentation/screens/otp/otp_screen.dart';
 import 'package:productive_families/presentation/screens/register/register_screen.dart';
 import 'package:productive_families/presentation/screens/shop_layout/shop_layout.dart';
-import 'package:productive_families/presentation/screens/start/start_screen.dart';
 import 'package:productive_families/presentation/screens/terms_and_conditions/terms_and_conditions.dart';
 
 class AppRouter {
   late Widget startWidget;
 
   AppRouter() {
-    startWidget = StartScreen();
-    // startWidget = LocationPicker();
+    // startWidget = StartScreen();
+    startWidget = FilteringScreen();
     // startWidget = OrdersScreen();
   }
 
